72f27a04b9571b88d724ea5d68a4e355f68bd92f
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 20:30:36 +0330

    new listeners

--- Download Series/background.js
+++ Download Series/background.js
@@ -7,28 +7,50 @@ chrome.action.onClicked.addListener((tab) => {
                     .map((a) => a.href)
                     .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
                 let linksStr = links.join("\n");
-
-                let success = false;
-                if (linksStr) {
-                    document.focus();
-                    navigator.clipboard.writeText(linksStr).then(() => {
-                        success = true;
-                        chrome.runtime.sendMessage({ success: true });
-                    });
-                } else {
-                    chrome.runtime.sendMessage({ success: false });
-                }
+                return linksStr;
             },
+        },
+        (results) => {
+            let linksStr = results[0];
+            if (linksStr) {
+                navigator.clipboard.writeText(linksStr).then(() => {
+                    chrome.action.setBadgeBackgroundColor({ color: "#0f0" });
+                    chrome.action.setBadgeText({ text: "✔️" });
+                });
+            } else {
+                chrome.action.setBadgeBackgroundColor({ color: "#f00" });
+                chrome.action.setBadgeText({ text: "😞" });
+            }
         }
     );
 });
 
-chrome.runtime.onMessage.addListener((message, sender) => {
-    if (message.success) {
-        chrome.action.setBadgeBackgroundColor({ color: "#0f0" });
-        chrome.action.setBadgeText({ text: "✔️" });
-    } else {
-        chrome.action.setBadgeBackgroundColor({ color: "#f00" });
-        chrome.action.setBadgeText({ text: "😞" });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'copyLinks') {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        let links = Array.from(document.querySelectorAll("a"))
+                            .map((a) => a.href)
+                            .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
+                        let linksStr = links.join("\n");
+                        return linksStr;
+                    },
+                },
+                (results) => {
+                    let linksStr = results[0];
+                    if (linksStr) {
+                        navigator.clipboard.writeText(linksStr).then(() => {
+                            sendResponse({ success: true });
+                        });
+                    } else {
+                        sendResponse({ success: false });
+                    }
+                }
+            );
+        });
     }
+    return true;
 });
