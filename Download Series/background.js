b99614c754af64b11f05f94e6f81738ba56dfc04
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 20:45:37 +0330

    fix API

--- Download Series/background.js
+++ Download Series/background.js
@@ -1,55 +1,49 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.executeScript(
-        tab.id,
-        {
-            code: `(() => {
-                let links = Array.from(document.querySelectorAll("a"))
-                    .map((a) => a.href)
-                    .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
-                let linksStr = links.join("\\n");
-                return linksStr;
-            })()`,
-        },
-        (results) => {
-            let linksStr = results[0];
-            if (linksStr) {
-                navigator.clipboard.writeText(linksStr).then(() => {
-                    chrome.action.setBadgeBackgroundColor({ color: "#0f0" });
-                    chrome.action.setBadgeText({ text: "✔️" });
-                });
-            } else {
-                chrome.action.setBadgeBackgroundColor({ color: "#f00" });
-                chrome.action.setBadgeText({ text: "😞" });
-            }
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            let links = Array.from(document.querySelectorAll("a"))
+                .map((a) => a.href)
+                .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
+            let linksStr = links.join("\\n");
+            return linksStr;
         }
-    );
+    }, (results) => {
+        let linksStr = results[0].result;
+        if (linksStr) {
+            navigator.clipboard.writeText(linksStr).then(() => {
+                chrome.action.setBadgeBackgroundColor({ color: "#0f0" });
+                chrome.action.setBadgeText({ text: "✔️" });
+            });
+        } else {
+            chrome.action.setBadgeBackgroundColor({ color: "#f00" });
+            chrome.action.setBadgeText({ text: "😞" });
+        }
+    });
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'copyLinks') {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {
-                    code: `(() => {
-                        let links = Array.from(document.querySelectorAll("a"))
-                            .map((a) => a.href)
-                            .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
-                        let linksStr = links.join("\\n");
-                        return linksStr;
-                    })()`,
-                },
-                (results) => {
-                    let linksStr = results[0];
-                    if (linksStr) {
-                        navigator.clipboard.writeText(linksStr).then(() => {
-                            sendResponse({ success: true });
-                        });
-                    } else {
-                        sendResponse({ success: false });
-                    }
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: () => {
+                    let links = Array.from(document.querySelectorAll("a"))
+                        .map((a) => a.href)
+                        .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
+                    let linksStr = links.join("\\n");
+                    return linksStr;
                 }
-            );
+            }, (results) => {
+                let linksStr = results[0].result;
+                if (linksStr) {
+                    navigator.clipboard.writeText(linksStr).then(() => {
+                        sendResponse({ success: true });
+                    });
+                } else {
+                    sendResponse({ success: false });
+                }
+            });
         });
     }
     return true;
