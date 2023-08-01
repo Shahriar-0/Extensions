197809a6d68e73b0a7ecefa6c77039fe47ecacd6
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 19:23:04 +0330

    fixed background

--- Download Series/background.js
+++ Download Series/background.js
@@ -1,17 +1,27 @@
-chrome.browserAction.onClicked.addListener(tab => {
-    chrome.tabs.executeScript(tab.id, {
-        code: `
-            let links = Array.from(document.querySelectorAll('a'))
-                .map(a => a.href)
-                .filter(href => href.endsWith('.mkv') || href.endsWith('.mp4'));
-            let linksStr = links.join('\\n');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'copyLinks') {
+        chrome.tabs.executeScript({
+            code: `
+                let links = Array.from(document.querySelectorAll('a'))
+                    .map(a => a.href)
+                    .filter(href => href.endsWith('.mkv') || href.endsWith('.mp4'));
+                let linksStr = links.join('\\n');
 
-            let textarea = document.createElement('textarea');
-            textarea.value = linksStr;
-            document.body.appendChild(textarea);
-            textarea.select();
-            document.execCommand('copy');
-            document.body.removeChild(textarea);
-        `
-    });
+                let success = false;
+                if (linksStr) {
+                    let textarea = document.createElement('textarea');
+                    textarea.value = linksStr;
+                    document.body.appendChild(textarea);
+                    textarea.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(textarea);
+                    success = true;
+                }
+                success
+            `
+        }, results => {
+            sendResponse({ success: results[0] });
+        });
+        return true;
+    }
 });
