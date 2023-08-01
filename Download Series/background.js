6c03f9847dc9f17df72fee6f4f0023d2fe708522
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 21:16:33 +0330

    deleted extra listener

--- Download Series/background.js
+++ Download Series/background.js
@@ -1,38 +1,3 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => {
-            let links = Array.from(document.querySelectorAll("a"))
-                .map((a) => a.href)
-                .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
-            let linksStr = links.join("\\n");
-            return linksStr;
-        }
-    }, (results) => {
-        let linksStr = results[0].result;
-        if (linksStr) {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: (linksStr) => {
-                    let textarea = document.createElement('textarea');
-                    textarea.value = linksStr;
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(textarea);
-                },
-                args: [linksStr]
-            }, () => {
-                chrome.action.setBadgeBackgroundColor({ color: "#0f0" });
-                chrome.action.setBadgeText({ text: "✔️" });
-            });
-        } else {
-            chrome.action.setBadgeBackgroundColor({ color: "#f00" });
-            chrome.action.setBadgeText({ text: "😞" });
-        }
-    });
-});
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'copyLinks') {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
