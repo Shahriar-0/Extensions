148e094ab06431435a61d02e42f5d16cb0ef9843
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 19:26:00 +0330

    manifest v3

--- Download Series/background.js
+++ Download Series/background.js
@@ -1,27 +1,33 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'copyLinks') {
-        chrome.tabs.executeScript({
-            code: `
-                let links = Array.from(document.querySelectorAll('a'))
-                    .map(a => a.href)
-                    .filter(href => href.endsWith('.mkv') || href.endsWith('.mp4'));
-                let linksStr = links.join('\\n');
+chrome.action.onClicked.addListener(tab => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            let links = Array.from(document.querySelectorAll('a'))
+                .map(a => a.href)
+                .filter(href => href.endsWith('.mkv') || href.endsWith('.mp4'));
+            let linksStr = links.join('\n');
 
-                let success = false;
-                if (linksStr) {
-                    let textarea = document.createElement('textarea');
-                    textarea.value = linksStr;
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(textarea);
-                    success = true;
-                }
-                success
-            `
-        }, results => {
-            sendResponse({ success: results[0] });
-        });
-        return true;
+            let success = false;
+            if (linksStr) {
+                let textarea = document.createElement('textarea');
+                textarea.value = linksStr;
+                document.body.appendChild(textarea);
+                textarea.select();
+                document.execCommand('copy');
+                document.body.removeChild(textarea);
+                success = true;
+            }
+            chrome.runtime.sendMessage({ success });
+        }
+    });
+});
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    if (message.success) {
+        chrome.action.setBadgeBackgroundColor({ color: '#0f0' });
+        chrome.action.setBadgeText({ text: '✔️' });
+    } else {
+        chrome.action.setBadgeBackgroundColor({ color: '#f00' });
+        chrome.action.setBadgeText({ text: '😞' });
     }
 });
