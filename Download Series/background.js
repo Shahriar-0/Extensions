f1cf802aae4bc417a3ea15c70a300c1535f1e642
Shahriar-0 <attarshahriar@gmail.com>
2023-08-01 22:00:56 +0330

    copy with selected file types

--- Download Series/background.js
+++ Download Series/background.js
@@ -1,15 +1,25 @@
+let category = 'movies';
+let fileTypes = ['mkv'];
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'copyLinks') {
+    if (message.action === 'setCategory') {
+        category = message.category;
+    } else if (message.action === 'setFileTypes') {
+        fileTypes = message.fileTypes;
+    } else if (message.action === 'copyLinks') {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
             chrome.scripting.executeScript({
                 target: { tabId: tabs[0].id },
-                function: () => {
+                function: (category, fileTypes) => {
                     let links = Array.from(document.querySelectorAll("a"))
                         .map((a) => a.href)
-                        .filter((href) => href.endsWith(".mkv") || href.endsWith(".mp4"));
+                        .filter((href) => {
+                            return fileTypes.some((fileType) => href.endsWith("." + fileType));
+                        });
                     let linksStr = links.join("\\n");
                     return linksStr;
-                }
+                },
+                args: [category, fileTypes]
             }, (results) => {
                 let linksStr = results[0].result;
                 if (linksStr) {
